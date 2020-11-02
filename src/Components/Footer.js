0eccf1070123930ebb10de6fcc0c4ec84ba5748d
Viktor <SuvorovViktorA@gmail.com>
2020-11-02 13:03:43 +0300

    Fixed styles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export default (props) => (
+    <>
     <div className='todo-footer'>
     <button onClick={props.updateTodoToShowAll}>All</button>
     <button onClick={props.updateTodoToShowActive}>Active</button>
     <button onClick={props.updateTodoToShowCompleted}>Completed</button>
     </div>
+    <div className='footer-info'>Double-click on text to edit a todo</div>
+    </>
     )
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,3 @@
 import React from "react";
 
-function Header() {
-  return <h1 className="header-h1">todos</h1>;
-}
-
-export default Header;
+export default () => <h1 className="header-h1">todos</h1>;
